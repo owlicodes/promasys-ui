dfba99bd06e832533bd5fbdc181299897bae8dee
Jose Diago <owlicodes@gmail.com>
2024-11-29 14:10:49 +0800

    Change some texts in the hero section.

--- src/features/public/components/hero-section.tsx
+++ src/features/public/components/hero-section.tsx
@@ -1,21 +1,37 @@
-import { GoogleSignInButton } from "@/features/auth/components/google-sign-in-button";
+import Link from "next/link";
 
-export const HeroSection = () => {
+import { getServerSession } from "next-auth";
+
+import { Button } from "@/components/ui/button";
+import { GoogleSignInButton } from "@/features/auth/components/google-sign-in-button";
+import { authOptions } from "@/lib/next-auth-config";
+
+export const HeroSection = async () => {
+  const session = await getServerSession(authOptions);
+
   return (
     <section className="w-full bg-gradient-to-r from-brand via-purple-500 to-pink-200 py-12 md:py-24 lg:py-32 xl:py-48">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="space-y-2">
-            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+            <h1 className="mb-4 text-3xl font-bold tracking-tight sm:text-4xl md:text-5xl lg:text-6xl/none">
               Manage Your Projects with Ease
             </h1>
-            <p className="mx-auto max-w-[700px] md:text-xl">
-              Streamline your workflow, collaborate effortlessly, and deliver
-              projects on time with promasys.
+            <p className="mx-auto max-w-[700px] tracking-tight md:text-xl">
+              Hi 👋, I am{" "}
+              <span className="font-bold text-yellow-400">Jose Diago</span>, and
+              I built this app to help you streamline your workflow, collaborate
+              effortlessly, and deliver projects on time with promasys.
             </p>
           </div>
           <div className="space-x-4">
-            <GoogleSignInButton>Sign In with Google</GoogleSignInButton>
+            {!session?.user ? (
+              <GoogleSignInButton>Start Exploring!</GoogleSignInButton>
+            ) : (
+              <Link href="/default">
+                <Button size="lg">Get Started</Button>
+              </Link>
+            )}
           </div>
         </div>
       </div>
--- src/features/public/components/invite-section.tsx
+++ src/features/public/components/invite-section.tsx
@@ -1,6 +1,14 @@
-import { GoogleSignInButton } from "@/features/auth/components/google-sign-in-button";
+import Link from "next/link";
 
-export const InviteSection = () => {
+import { getServerSession } from "next-auth";
+
+import { Button } from "@/components/ui/button";
+import { GoogleSignInButton } from "@/features/auth/components/google-sign-in-button";
+import { authOptions } from "@/lib/next-auth-config";
+
+export const InviteSection = async () => {
+  const session = await getServerSession(authOptions);
+
   return (
     <section className="w-full bg-brand py-12 text-white md:py-24 lg:py-32">
       <div className="container px-4 md:px-6">
@@ -11,13 +19,25 @@ export const InviteSection = () => {
             </h2>
             <p className="mx-auto max-w-[600px] md:text-xl">
               Join thousands of teams already using promasys to streamline their
-              project management. Just kidding, I don&apos;t have thousands of
-              users, but maybe join me in using promasys.
+              project management.{" "}
+              <span className="italic">
+                Just kidding, I don&apos;t have thousands of users, but maybe
+                join me in using promasys
+              </span>
+              .
             </p>
           </div>
-          <GoogleSignInButton variant="outline" className="text-brand">
-            Sign In with Google
-          </GoogleSignInButton>
+          {!session?.user ? (
+            <GoogleSignInButton variant="outline" className="text-brand">
+              Sign In with Google
+            </GoogleSignInButton>
+          ) : (
+            <Link href="/default">
+              <Button size="lg" variant="outline" className="text-brand">
+                Get Started
+              </Button>
+            </Link>
+          )}
         </div>
       </div>
     </section>
