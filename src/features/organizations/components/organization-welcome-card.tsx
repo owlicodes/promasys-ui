37af281bc686ca523aafed189530c2cb81c2b7c9
Jose Diago <owlicodes@gmail.com>
2024-12-02 15:13:07 +0800

    More renaming promasys.

--- src/features/organizations/components/organization-welcome-card.tsx
+++ src/features/organizations/components/organization-welcome-card.tsx
@@ -5,7 +5,7 @@ export const OrganizationWelcomeCard = () => {
   return (
     <div className="space-y-4">
       <h1 className="text-3xl font-bold">
-        Welcome to <span className="text-brand">promasys</span>!
+        Welcome to <span className="text-brand">proyektibo</span>!
       </h1>
       <p>Let&apos;s get started by choosing 1 of the following actions.</p>
       <div className="flex gap-4">
