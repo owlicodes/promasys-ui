eb93b28f5ce38dba0d67b7ce1f0a4746150280be
Jose Diago <owlicodes@gmail.com>
2024-12-06 19:22:55 +0800

    Creating the bread crumb for create work item page.

--- src/app/(organization)/[organization]/[projectId]/create-work-item/page.tsx
+++ src/app/(organization)/[organization]/[projectId]/create-work-item/page.tsx
@@ -1,7 +1,12 @@
+import { CreateWorkItemBreadcrumb } from "@/features/work-items/components/create-work-item-breadcrumb";
+
 export default function CreateWorkItem() {
   return (
     <div>
-      <h1>Create Work Item Page</h1>
+      <CreateWorkItemBreadcrumb />
+      <div className="mt-6">
+        <h1>Create Work Item Form Here...</h1>
+      </div>
     </div>
   );
 }
---
+++ src/features/work-items/components/create-work-item-breadcrumb.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { useProjectDetails } from "@/features/projects/apis/use-project-details";
+import { useSprintDetails } from "@/features/sprints/apis/use-sprint-details";
+import useSelectedOrganizationStore from "@/stores/selected-organization-store";
+
+export const CreateWorkItemBreadcrumb = () => {
+  const { selectedOrganization } = useSelectedOrganizationStore();
+  const { projectId, sprintId } = useParams<{
+    projectId: string;
+    sprintId: string;
+  }>();
+  const project = useProjectDetails(projectId);
+  const sprint = useSprintDetails(projectId, sprintId);
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href={`/${selectedOrganization?.name}`}>
+              {selectedOrganization?.name}
+            </Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href={`/${selectedOrganization?.name}/${project.data?.id}`}>
+              {project.data?.name}
+            </Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        {sprintId && (
+          <>
+            <BreadcrumbItem>
+              <BreadcrumbLink asChild>
+                <Link
+                  href={`/${selectedOrganization?.name}/${project.data?.id}/${sprint.data?.id}`}
+                >
+                  {sprint.data?.name}
+                </Link>
+              </BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+          </>
+        )}
+        <BreadcrumbItem>
+          <BreadcrumbPage>Create Work Item</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
