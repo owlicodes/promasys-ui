225671dfea2e5f18282a0ede2fe46a57b0d20491
Jose Diago <owlicodes@gmail.com>
2024-12-09 14:57:08 +0800

    Fixing the spacing of cards.

--- src/features/sprints/components/sprints-list.tsx
+++ src/features/sprints/components/sprints-list.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DataTable } from "@/features/common/components/data-table";
 
 import { TSprint } from "../sprint-schema";
@@ -6,8 +6,10 @@ import { sprintColumns } from "./sprint-columns";
 
 export const SprintsList = ({ data }: { data: TSprint[] }) => {
   return (
-    <Card className="space-y-6 p-6">
-      <CardTitle className="text-2xl font-bold">Project Sprints</CardTitle>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold">Project Sprints</CardTitle>
+      </CardHeader>
       <CardContent>
         <DataTable columns={sprintColumns} data={data} />
       </CardContent>
--- src/features/work-items/components/work-items-list.tsx
+++ src/features/work-items/components/work-items-list.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DataTable } from "@/features/common/components/data-table";
 
 import { TWorkItem } from "../work-item-schemas";
@@ -6,8 +6,10 @@ import { workItemColumns } from "./work-item-columns";
 
 export const WorkItemsList = ({ data }: { data: TWorkItem[] }) => {
   return (
-    <Card className="space-y-6 p-6">
-      <CardTitle className="text-2xl font-bold">Work Items</CardTitle>
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold">Work Items</CardTitle>
+      </CardHeader>
       <CardContent>
         <DataTable columns={workItemColumns} data={data} />
       </CardContent>
